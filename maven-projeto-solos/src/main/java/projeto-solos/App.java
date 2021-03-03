357b26815ccda0860111ca7b7e73ebe8e6c19066
Nicole Beatriz <nick_beatriz_@hotmail.com>
2021-03-03 03:33:09 -0300

    add projeto base

---
+++ maven-projeto-solos/src/main/java/projeto-solos/App.java
@@ -0,0 +1,13 @@
+package projeto-solos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
