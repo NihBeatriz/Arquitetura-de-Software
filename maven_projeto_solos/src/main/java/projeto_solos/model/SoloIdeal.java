400a72913e8b58d136b0697983c1a0ba9923f960
Nicole Beatriz <nick_beatriz_@hotmail.com>
2021-03-03 16:04:45 -0300

    add model de Solo Ideal

---
+++ maven_projeto_solos/src/main/java/projeto_solos/model/SoloIdeal.java
@@ -0,0 +1,131 @@
+package projeto_solos.model;
+
+public class SoloIdeal {
+
+    private double fosforoIdeal;
+    private double potassioIdeal;
+    private double calcioIdeal;
+    private double magnesioIdeal;
+    private double enxofreIdeal;
+    private double aluminioIdeal;
+    private int texturaSolo;
+
+    public double getFosforoIdeal() {
+        return fosforoIdeal;
+    }
+
+    public void setFosforoIdeal(double fosforoIdeal) {
+        this.fosforoIdeal = fosforoIdeal;
+    }
+
+    public double getPotassioIdeal() {
+        return potassioIdeal;
+    }
+
+    public void setPotassioIdeal(double potassioIdeal) {
+        this.potassioIdeal = potassioIdeal;
+    }
+    public double getCalcioIdeal() {
+        return calcioIdeal;
+    }
+
+    public void setCalcioIdeal(double calcioIdeal) {
+        this.calcioIdeal = calcioIdeal;
+    }
+    public double getMagnesioIdeal() {
+        return magnesioIdeal;
+    }
+
+    public void setMagnesioIdeal(double magnesioIdeal) {
+        this.magnesioIdeal = magnesioIdeal;
+    }
+    public double getEnxofreIdeal() {
+        return enxofreIdeal;
+    }
+
+    public void setEnxofreIdeal(double enxofreIdeal) {
+        this.enxofreIdeal = enxofreIdeal;
+    }
+
+    public double getAluminioIdeal() {
+        return aluminioIdeal;
+    }
+
+    public void setAluminioIdeal(double aluminioIdeal) {
+        this.aluminioIdeal = aluminioIdeal;
+    }
+
+    public double getTexturaSolo() {
+        return texturaSolo;
+    }
+
+    public void setTexturaSolo(int texturaSolo) {
+        this.texturaSolo = texturaSolo;
+    }
+
+    public double fosforoIdeal(int texturaSolo) {
+        switch(texturaSolo) {
+            case 1:
+                return 9.0;
+            case 2:
+                return 12.0;
+            default:
+                return 0.0;
+        }
+    }
+
+    public double potassioIdeal(int texturaSolo) {
+        switch(texturaSolo) {
+            case 1:
+                return 0.35;
+            case 2:
+                return 0.25;
+            default:
+                return 0.0;
+        }
+    }
+
+    public double calcioIdeal(int texturaSolo) {
+        switch(texturaSolo) {
+            case 1:
+                return 6.0;
+            case 2:
+                return 4.0;
+            default:
+                return 0.0;
+        }
+    }
+
+    public double magnesioIdeal(int texturaSolo) {
+        switch(texturaSolo) {
+            case 1:
+                return 1.5;
+            case 2:
+                return 1.0;
+            default:
+                return 0.0;
+        }
+    }
+
+    public double enxofreIdeal(int texturaSolo) {
+        switch(texturaSolo) {
+            case 1:
+                return 9.0;
+            case 2:
+                return 6.0;
+            default:
+                return 0.0;
+        }
+    }
+
+    public double aluminioIdeal(int texturaSolo) {
+        switch(texturaSolo) {
+            case 1:
+                return 0.0;
+            case 2:
+                return 0.0;
+            default:
+                return 0.0;
+        }
+    }
+}
