0ab9353ac3c069a8aa3964e40a36d5a776a88945
Nicole Beatriz <nick_beatriz_@hotmail.com>
2021-03-19 16:51:52 -0300

    update no método verificaSolo na classe SoloIdeal

--- maven_projeto_solos/src/main/java/projeto_solos/model/SoloIdeal.java
+++ maven_projeto_solos/src/main/java/projeto_solos/model/SoloIdeal.java
@@ -2,91 +2,98 @@ package projeto_solos.model;
 
 public class SoloIdeal {
 
-    public double fosforoIdeal(String texturaSolo) {
+    public double fosforoIdeal(int texturaSolo) {
         switch(texturaSolo) {
-            case "1":
+            case 1:
                 return 9.0;
-            case "2":
+            case 2:
                 return 12.0;
             default:
                 return 0.0;
         }
     }
 
-    public double potassioIdeal(String texturaSolo) {
+    public double potassioIdeal(int texturaSolo) {
         switch(texturaSolo) {
-            case "1":
+            case 1:
                 return 0.35;
-            case "2":
+            case 2:
                 return 0.25;
             default:
                 return 0.0;
         }
     }
 
-    public double calcioIdeal(String texturaSolo) {
+    public double calcioIdeal(int texturaSolo) {
         switch(texturaSolo) {
-            case "1":
+            case 1:
                 return 6.0;
-            case "2":
+            case 2:
                 return 4.0;
             default:
                 return 0.0;
         }
     }
 
-    public double magnesioIdeal(String texturaSolo) {
+    public double magnesioIdeal(int texturaSolo) {
         switch(texturaSolo) {
-            case "1":
+            case 1:
                 return 1.5;
-            case "2":
+            case 2:
                 return 1.0;
             default:
                 return 0.0;
         }
     }
 
-    public double enxofreIdeal(String texturaSolo) {
+    public double enxofreIdeal(int texturaSolo) {
         switch(texturaSolo) {
-            case "1":
+            case 1:
                 return 9.0;
-            case "2":
+            case 2:
                 return 6.0;
             default:
                 return 0.0;
         }
     }
 
-    public double aluminioIdeal(String texturaSolo) {
+    public double aluminioIdeal(int texturaSolo) {
         switch(texturaSolo) {
-            case "1":
+            case 1:
                 return 0.0;
-            case "2":
+            case 2:
                 return 0.0;
             default:
                 return 0.0;
         }
     }
 
-    public double hAlIdeal(String texturaSolo) {
+    public double hAlIdeal(int texturaSolo) {
         switch(texturaSolo) {
-            case "1":
+            case 1:
                 return 0.0;
-            case "2":
+            case 2:
                 return 0.0;
             default:
                 return 0.0;
         }
     }
 
-    public void verificaSolo(String texturaSolo) {    
-        if(texturaSolo.equals("1")) {
-            System.out.println("\nSolo Argiloso\n");
-        } else if(texturaSolo.equals("2")) {
-            System.out.println("\nSolo Textura Média\n");
-        } else {
-            System.out.println("\nValor Inválido!\n");
-            System.exit(0);
-        }
+    public String verificaSolo(int texturaSolo) {   
+        String tipoSolo;
+
+        switch(texturaSolo) {
+            case 1:
+                tipoSolo = "Solo Argiloso";
+                break;
+            case 2:
+                tipoSolo = "Solo Textura Média";
+                break;
+            default:
+                tipoSolo = "Valor de Solo Inválido";
+                break;
+        } 
+
+        return tipoSolo;
     }
 }
