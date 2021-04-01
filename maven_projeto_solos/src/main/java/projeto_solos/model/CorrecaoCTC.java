575363627430157864a3d292a7f32cb3b36f6c65
Nicole Beatriz <nick_beatriz_@hotmail.com>
2021-04-01 00:07:37 -0300

    add correção do CTC

---
+++ maven_projeto_solos/src/main/java/projeto_solos/model/CorrecaoCTC.java
@@ -0,0 +1,41 @@
+package projeto_solos.model;
+
+public class CorrecaoCTC {
+    
+    private double sCmol;
+    private double ctcCmol;
+
+    public double calculoSCmol(double potassio, double calcio, double magnesio) {
+        sCmol = potassio + calcio + magnesio;
+        return sCmol;
+    }
+
+    public double calculoCTCCmol(double sCmol, double hAl) {
+        ctcCmol = sCmol + hAl;
+        return ctcCmol;
+    }
+
+    public double calculoVPercentual(double sCmol, double ctcCmol) {
+        if(sCmol > 0 && ctcCmol > 0) {
+            return (sCmol * 100) / ctcCmol;
+        } else {
+            return 0.0;
+        }
+    }
+
+    public double calculoMoPercentual(double mo) {
+        if(mo > 0) {
+            return mo / 10;
+        } else {
+            return 0.0;
+        }
+    }
+
+    public double calculoCarbono(double moPercentual) {
+        if(moPercentual > 0) {
+            return (moPercentual / 1.72) * 10;
+        } else {
+            return 0.0;
+        }
+    }
+}
