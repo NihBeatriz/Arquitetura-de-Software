4de01738639e99e5f79fdb2a098b24d9a5af7fe8
Nicole Beatriz <nick_beatriz_@hotmail.com>
2021-04-18 15:23:34 -0300

    add Correção de Potássio

---
+++ maven_projeto_solos/src/main/java/projeto_solos/model/CorrecaoPotassio.java
@@ -0,0 +1,40 @@
+package projeto_solos.model;
+
+public class CorrecaoPotassio {
+
+    public double calculoNecessidadePotassio(double potassioAtual, double potassioDesejado, double potassioCTC) {
+        return (potassioAtual * potassioDesejado / potassioCTC) - potassioAtual;
+    }
+
+    public double calculoCmolCm3KEmMgDm3(double calculoNecessidadePotassio) {
+        if (calculoNecessidadePotassio > 0.01) {
+            return calculoNecessidadePotassio * 39.1 * 10;
+        }
+
+        return 0.0;
+    }
+
+    public double calculoMgDm3EmKgHa(double calculoCmolCm3KEmMgDm3) {
+        return calculoCmolCm3KEmMgDm3 * 2;
+    }
+
+    public double calculoKgHaEmK2O(double calculoMgDm3EmKgHa) {
+        return calculoMgDm3EmKgHa * 1.2;
+    }
+
+    public double calculoEficienciaPotassio(double calculoKgHaEmK2O, double eficienciaPotassio) {
+        return calculoKgHaEmK2O / eficienciaPotassio;
+    }
+
+    public double calculoQuantidadeAplicar(double calculoEficienciaPotassio, double fontePotassio) {
+        return calculoEficienciaPotassio * 100 / fontePotassio;
+    }
+
+    public double calculoQuantidadeAplicarKgHaEmKgAlqueire(double calculoQuantidadeAplicar) {
+        return calculoQuantidadeAplicar * 2.42;
+    }
+    
+    public double calculoCustoPorAlqueire(double precoFontePotassio, double calculoQuantidadeAplicarKgHaEmKgAlqueire){
+        return ((precoFontePotassio * calculoQuantidadeAplicarKgHaEmKgAlqueire) / 1000) / 2.42;
+    }
+}
