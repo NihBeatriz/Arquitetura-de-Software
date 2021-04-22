422215b77ee783c998f93c5dce1760da19ba5b64
Nicole Beatriz <nick_beatriz_@hotmail.com>
2021-04-22 20:53:33 -0300

    add refatoração do enum Fonte de Fósforo

--- maven_projeto_solos/src/main/java/projeto_solos/enums/FonteFosforo.java
+++ maven_projeto_solos/src/main/java/projeto_solos/enums/FonteFosforo.java
@@ -2,66 +2,26 @@ package projeto_solos.enums;
 
 public enum FonteFosforo {
 
-    SUPERFOSFATO_SIMPLES {
-        public double valor() {
-            return 18.0;
-        }
-    },
-    SUPERFOSFATO_TRIPLO {
-        public double valor() {
-            return 41.0;
-        }
-    },
-    MAP {
-        public double valor() {
-            return 48.0;
-        }
-    },
-    DAP {
-        public double valor() {
-            return 45.0;
-        }
-    },
-    YOORIN {
-        public double valor() {
-            return 18.0;
-        }
-    },
-    FOSFATO_ARAD {
-        public double valor() {
-            return 33.0;
-        }
-    },
-    FOSFATO_GAFSA {
-        public double valor() {
-            return 29.0;
-        }
-    },
-    FOSFATO_DAOUI {
-        public double valor() {
-            return 32.0;
-        }
-    },
-    FOFATO_PATOS_MINAS {
-        public double valor() {
-            return 24.0;
-        }
-    },
-    ESCORIA_DE_THOMAS {
-        public double valor() {
-            return 18.5;
-        }
-    },
-    ACIDO_FOSFORICO{
-        public double valor() {
-            return 52.0;
-        }
-    },
-    MULTIFOSFATO_MAGNESIANO {
-        public double valor() {
-            return 18.0;
-        }
-    };
-    
-    public abstract double valor();
+    SUPERFOSFATO_SIMPLES(18.0), 
+    SUPERFOSFATO_TRIPLO(41.0),
+    MAP(48.0),
+    DAP(45.0),
+    YOORIN(18.0),
+    FOSFATO_ARAD(33.0),
+    FOSFATO_GAFSA(29.0),
+    FOSFATO_DAOUI(32.0),
+    FOFATO_PATOS_MINAS(24.0),
+    ESCORIA_DE_THOMAS(18.5),
+    ACIDO_FOSFORICO(52.0),
+    MULTIFOSFATO_MAGNESIANO(18.0);
+
+    private final double valor;
+
+    private FonteFosforo(double valor) {
+        this.valor = valor;
+    }
+        
+    public double getValor(){
+        return this.valor;
+    }
 }
